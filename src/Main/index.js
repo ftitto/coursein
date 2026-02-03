2dbec2273f583b681bd176e852f61858c8c3b32a
Francisco Ferreira <franciscoftitto@gmail.com>
2026-02-02 23:58:25 -0300

    primeiro commit

---
+++ src/Main/index.js
@@ -0,0 +1,15 @@
+function Main() {
+    return (
+
+        <main>
+            <div className = 'Div-Main'>
+                <span>Olá, espero que esteja bem! <br/>
+                    Este é o esboço inicial do nosso projeto! <br/>
+                    Sinta-se à vontade para explorar e nos dar um feedback de como está sendo a experiência!
+                </span>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
