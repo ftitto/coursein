8dbd3898a26424cb54b19ae0cbb9922558ae3df8
Francisco Ferreira <franciscoftitto@gmail.com>
2026-02-03 00:59:24 -0300

    Some changes

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,9 +3,14 @@ function Main() {
 
         <main>
             <div className = 'Div-Main'>
-                <span>Olá, espero que esteja bem! <br/>
-                    Este é o esboço inicial do nosso projeto! <br/>
-                    Sinta-se à vontade para explorar e nos dar um feedback de como está sendo a experiência!
+                <span className = 'Span-Main'>
+                    Bem-vindo ao CourseIn!
+                </span>
+
+                <span className ='Span-presentation'>
+                    A minha ideia aqui, foi recriar um dos meus projetos, usando <strong className ='strong'>ReactJS.</strong> <br/>
+                    O CourseIn foi criado para ser uma plataforma que ofecere cursos gratuitos online, com o objetivo de democratizar o acesso ao conhecimento e capacitar pessoas de diferentes origens a adquirirem novas habilidades. <br/>
+                    O projeto estará em constante evolução, onde adicionarei novas funcionalidades, cursos e irei aprimorar a experiência do usuário com o tempo.
                 </span>
             </div>
         </main>
